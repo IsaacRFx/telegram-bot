93c4d8ced16bb9b02ea344dbaceaa9a0dea9484d
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-10-22 15:06:04 -0700

    debug: Requests not working in production

--- devbot.py
+++ devbot.py
@@ -2,6 +2,7 @@ from ctypes import cast
 import logging
 from html import unescape
 import os
+from time import time
 from telegram import (
     Update,
     InlineQueryResultArticle,
@@ -91,7 +92,7 @@ async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info("Question of %s: %s", user.first_name, update.message.text)
     query = {"query": userQuestion}
     logger.info("Query: %s", query)
-    reply = requests.post(f'{config("WEBHOOK_URL", "")}api/scrape/', json=query).json()
+    reply = requests.post(f'{config("WEBHOOK_URL", "")}api/scrape/', json=query, timeout=10).json()
     logger.info("Request: %s", str(reply))
     readable_reply = str(reply['results'])
     await update.message.reply_text(text = readable_reply, parse_mode = ParseMode.HTML)
